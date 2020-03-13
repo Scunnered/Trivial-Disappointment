2f95b1a840fb7b208aa995212acf6c9097804ca1
Kacper <kacper.95.95@gmail.com>
2020-03-13 12:32:17 +0000

    fixed now for real I promise

--- prototype/server.js
+++ prototype/server.js
@@ -16,11 +16,9 @@ var qCounter = 0;
 
 app.post('/Join_Host_Game.html', function (req, res) {
     console.log("we did it reddit")
-    console.log(req)
-    console.log(req.body)
-    //url1 = createURL(req.body.AMOUNT, req.body.DIFFICULTY, req.body.CATEGORY)
-    //QAPIRESPONSE = getQuestions(url1);
-    //res.send(QAPIRESPONSE[qCounter])
+    url1 = createURL(req.body.AMOUNT, req.body.DIFFICULTY, req.body.CATEGORY)
+    QAPIRESPONSE = getQuestions(url1);
+    res.send(QAPIRESPONSE[qCounter])
 })
 
 function getQuestions(url1) {
