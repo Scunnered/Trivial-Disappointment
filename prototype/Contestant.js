c74d27b3a26c9146e4bc2258bc3333141108a826
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-03-19 14:59:46 +0000

    Update Contestant.js

--- prototype/Contestant.js
+++ prototype/Contestant.js
@@ -3,3 +3,9 @@ class Contestant {
 //Created this for testing. Here players will be able to add custom names in lieu of assigned ones which is the plan.
       this._name = name;
       this._colour = colour;
+      this._ready = false;
+//Players will say when they are ready to join in a host game. Once all connected users press ready the host can begin the game.
+}
+  
+  get name() {return this._name; }
+  get colour
