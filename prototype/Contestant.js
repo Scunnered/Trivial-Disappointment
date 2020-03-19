39eecedea77379a5af7f9fe947dd3f18e9a97b36
Ross M <44169316+Scunnered@users.noreply.github.com>
2020-03-19 14:55:44 +0000

    Update Contestant.js

--- prototype/Contestant.js
+++ prototype/Contestant.js
@@ -1,2 +1,5 @@
 class Contestant {
-  constructor 
+  constructor(name = "Name", colour = 'black') {
+//Created this for testing. Here players will be able to add custom names in lieu of assigned ones which is the plan.
+      this._name = name;
+      this._colour = colour;
