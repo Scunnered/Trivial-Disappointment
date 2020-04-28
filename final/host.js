bcdee3ac69a42ff67ae1ff2e9cbf7591324ea48d
plpkacper <43370662+plpkacper@users.noreply.github.com>
2020-04-28 15:28:26 +0100

    forcing db into a function

--- final/host.js
+++ final/host.js
@@ -36,10 +36,10 @@ class Host{
         var url1 = this.createURL(selects.AMOUNT, selects.DIFFICULTY, selects.CATEGORY)
         console.log(url1)
         const url = "mongodb://localhost:27017/coloured-animals";
-        MongoClient.connect(url, function(err, database){
+        MongoClient.connect(url, function(err, database, db){
             if(err) throw err;
-            this.db = database;
-        });
+            db = database;
+        }), this.db;
         //your get questions may take a while to retun so we can immediatly emit anything or create your array.
         //what we do is create a call back function...
         this.getQuestions(url1, function(returnedQs, hostObject){
